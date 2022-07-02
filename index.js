efa0091c6829fa394db8bb0a65dbea3c0af31bce
Baptiste <lantranbaptiste@gmail.com>
2022-07-02 16:47:15 +0200

    first commit contenant l'affichage des livres pour le moment avec le templating et un bout de code pour ajouter un livre

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const User = require('./models/User');
+const Book = require('./models/Book');
+const express = require('express');
+const session = require('express-session');
+const app = express();
+const ejs = require('ejs');
+const port = 3000;
+const path = require('path');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(session({ 
+    secret: '2e3740c7c0968bce0754a08451b70ea66ed6bd8baad1642b54035a1937e1b424',
+    resave: false,
+    cookie: { maxAge: 60000 },
+    saveUninitialized: true
+}))
+app.set('view engine','ejs');
+
+app.listen(port,function(){
+    console.log(`Serveur Express lancé sur le port : ${port}`)
+})
+
+
+app.get('/', async function(request, response) {
+
+    await mongoose.connect('mongodb://localhost:27017/books');
+
+
+    /*const book = new Book({
+        titre: 'Les 4 animaux',
+        auteur: 'Baptiste LT',
+        type: 'Thriller',
+        nbPages: 95,
+    })
+    book.save()
+        .then((result)=>{
+            //Logiquement redirect vers la page avec l'ID
+            response.send(result)
+        })
+        .catch((err)=>{
+            console.log(err)
+        });
+*/
+    const books = await Book.find({});
+
+
+
+    mongoose.connection.close()
+
+    const data = {books:books};
+    response.render('pages/home',data);
+});
 
 async function main(){
     try
     {
-        await mongoose.connect('mongodb://localhost:27017/demo');
+        await mongoose.connect('mongodb://localhost:27017/books');
 
         //const users = await User.find({nom: 'Cauchon'});
         const users = await User.find({$or:[
---
+++ models/Book.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const booksSchema = new Schema({
+    titre:{type:String, required: true},
+    auteur:{type: String, required: true},
+    type:{type: String, required: true},
+    nbPages: {type: Number, min: 0, required: true},
+},
+{
+    timestamp: true
+});
+
+const Book = mongoose.model('books', booksSchema)
+module.exports = Book;
